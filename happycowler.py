8346228227baccc217a920728ce926e8366493ea
Peter Wittek <peterwittek@users.noreply.github.com>
2016-08-06 18:17:39 +0200

    Updated to current version of HappyCow

--- happycowler.py
+++ happycowler.py
@@ -6,32 +6,24 @@ Created on Fri Sep 12 12:52:20 2014
 """
 from __future__ import print_function
 try:
-    from urllib2 import urlopen
+    from urllib2 import Request, urlopen
 except ImportError:
-    from urllib.request import urlopen
+    from urllib.request import Request, urlopen
+import random
 import sys
 from time import sleep
 from bs4 import BeautifulSoup
 
-def process_tags(tag_candidates):
-    tag='Other'
-    for tag_candidate in tag_candidates:
-        if tag_candidate.find('li',class_='vegan') is not None:
-            tag='Vegan'
-            break
-        if tag_candidate.find('li',class_='vegetarian') is not None:
-            tag='Vegetarian'
-            break
-    return tag
-    
+
 def parse_restaurant_page(restaurant_url):
-    restaurant_page = urlopen(restaurant_url)
-    parsed_restaurant_html = BeautifulSoup(restaurant_page)
-    gmap_link=parsed_restaurant_html.find('div', class_='map-holder').find('a')
-    text_link=gmap_link.find('img').get('src')
-    start=text_link.find('center=')+7
-    end=text_link.find('&')
-    coord2, coord1 = text_link[start:end].split(',')
+    req = Request(restaurant_url, headers={'User-Agent': 'Mozilla/5.0'})
+    restaurant_page = urlopen(req).read()
+    parsed_restaurant_html = BeautifulSoup(restaurant_page, "html.parser")
+    gmap_link = parsed_restaurant_html.find("div",
+                                            class_="box-map-links").find('a')
+    text_link = gmap_link.attrs["href"]
+    start=text_link.find('q=loc:')+6
+    coord1, coord2 = text_link[start:].split('+')
     return coord1+','+coord2
 
 def write_header(target_file):
@@ -51,28 +43,29 @@ def append_results_to_file(target_file, coordinates, names, tags, ratings, addre
     f = open(target_file, 'a')
     for i in range(len(names)):
         f.write('  <Placemark>\n')
-        f.write('    <name>' + names[i].encode('utf-8') + '</name>\n')
+        f.write('    <name>' + names[i] + '</name>\n')
         f.write('    <Point>\n')
         f.write('      <coordinates>' + coordinates[i] + ',0</coordinates>\n')
         f.write('    </Point>\n')
         f.write('   <description>\n')
         f.write('     <![CDATA[ ')
-        f.write(addresses[i].encode('utf-8') + '<br>')
-        f.write('<b>Phone:</b> ' + phone_numbers[i].encode('utf-8') + '<br>')
-        f.write('<b><u>Type</u>:</b> ' + tags[i].encode('utf-8') + ' ')
-        f.write('<b><u>Rating</u>:</b> ' + ratings[i].encode('utf-8') + '<br>')
-        f.write('<b><u>Cuisine</u>:</b> ' + cuisines[i].encode('utf-8') + '<br>')
-        f.write('<b><u>Business hours</u>:</b><br> ' + opening_hours[i].encode('utf-8') + '<br>')
-        f.write('<b><u>Description</u>:</b><br> ' + descriptions[i].encode('utf-8') + '<br>')
+        f.write(addresses[i] + '<br>')
+        f.write('<b>Phone:</b> ' + phone_numbers[i] + '<br>')
+        f.write('<b><u>Type</u>:</b> ' + tags[i] + ' ')
+        f.write('<b><u>Rating</u>:</b> ' + ratings[i] + '<br>')
+        f.write('<b><u>Cuisine</u>:</b> ' + cuisines[i] + '<br>')
+        f.write('<b><u>Business hours</u>:</b><br> ' + opening_hours[i] + '<br>')
+        f.write('<b><u>Description</u>:</b><br> ' + descriptions[i] + '<br>')
         f.write(' ]]>\n')
         f.write('   </description>\n')
         f.write('  </Placemark>\n')
     f.close()
 
 def parse_results_page(results_url, page_no, target_file):
-    page = urlopen(results_url + page_no)
-    parsed_html = BeautifulSoup(page)
-    
+    req = Request(results_url + page_no, headers={'User-Agent': 'Mozilla/5.0'})
+    page = urlopen(req).read()
+    parsed_html = BeautifulSoup(page, "html.parser")
+
     coordinates = []
     names = []
     tags = []
@@ -82,49 +75,69 @@ def parse_results_page(results_url, page_no, target_file):
     opening_hours = []
     cuisines = []
     descriptions = []
-    for business in parsed_html.body.findAll('article', class_='business-listing'):
-        restaurant_url = 'http://www.happycow.net' + business.find('h3').find('a').get('href')
+    for business in parsed_html.body.findAll('div', class_="row venue-list-item fg-form"):
+        restaurant_url = 'http://www.happycow.net' + business.find('a').get('href')
         coordinates.append(parse_restaurant_page(restaurant_url))
-        names.append(business.find('h3').text)
-        tags.append(process_tags(business.findAll('ul', class_='tags')))
-        ratings.append(business.find('span',class_='rating').text[:3])
-        address = business.find('address')
+        names.append(business.find('a').text)
+        tags.append(business.find('span').text)
+        stars = business.find('ul', class_="venue-ratings list-inline")
+        if stars is None:
+            rating = "unknown"
+        else:
+            rating = str(len(stars.findAll("i", class_="fa fa-star")) + 0.5*
+                         len(stars.findAll("i", class_="fa fa-star-half-o")))
+        ratings.append(rating)
+        address = None
+        for p in business.findAll('p'):
+           if p.find('i', class_="fa fa-map-marker"):
+               address = p.text
         if address is not None:
-            addresses.append(address.text)
+            addresses.append(address)
         else:
             addresses.append('')
-        phone_number = business.find('div', class_='phone')
+        phone_number = None
+        for p in business.findAll('p'):
+           if p.find('i', class_="fa fa-phone"):
+               phone_number = p.text
         if phone_number is not None:
-            phone_numbers.append(phone_number.text)
+            phone_numbers.append(phone_number)
         else:
             phone_numbers.append('')
-        opening_hour = business.find('div', class_='time')
+        opening_hour = business.find('span', class_='venue-hours-container')
         if opening_hour is not None:
-            opening_hours.append(opening_hour.text)
+            opening_hours.append(opening_hour.attrs['data-summary'])
         else:
             opening_hours.append('')
-        cuisine = business.find('em')
+
+        cuisine = None
+        for p in business.findAll('p'):
+           if "uisine" in p.text:
+               cuisine = p.text
         if cuisine is not None:
-            cuisines.append(cuisine.text[9:])
+            cuisines.append(cuisine)
         else:
             cuisines.append('')
-        description = business.find('p')
+        divs = business.findAll("div", class_="col-xs-12")
+        description = divs[-1].find('p').text
         if description is not None:
-            descriptions.append(description.text)
+            descriptions.append(description)
         else:
             description.append('')
-        sleep(1)
+        sleep(0.5 + random.random())
 
-    append_results_to_file(target_file, coordinates, names, tags, ratings, addresses, phone_numbers, opening_hours, cuisines, descriptions)
-    pager = parsed_html.find('span',class_='cpaging')
-    next_page = None
-    if pager is not None:
-      next_page = parsed_html.find('a', class_='next')
-    if next_page is not None:
-        parse_results_page(results_url, next_page.get('href'), target_file)
+    append_results_to_file(target_file, coordinates, names, tags, ratings,
+                           addresses, phone_numbers, opening_hours, cuisines,
+                           descriptions)
+    pagination = parsed_html.body.find('ul', class_="pagination").findAll('a')
+    for a in pagination:
+        if 'aria-label' in a.attrs and a.attrs['aria-label'] == "Next":
+            new_url = "http://www.happycow.net" + a.attrs['href']
+            if results_url + page_no != new_url:
+                parse_results_page("http://www.happycow.net", a.attrs['href'], target_file)
+
 
 if len(sys.argv) != 3:
-    print("Usage: python2 happycowler.py results_url output_file")
+    print("Usage: python happycowler.py results_url output_file")
     exit(-1)
 result_page = sys.argv[1]
 target_file = sys.argv[2]
