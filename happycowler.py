c38d528ff194a9c206f249d0b8d4337b9c033fc6
Peter Wittek <peterwittek@users.noreply.github.com>
2016-08-06 18:55:32 +0200

    Add GPX support for Osmand

--- happycowler.py
+++ happycowler.py
@@ -15,37 +15,66 @@ from time import sleep
 from bs4 import BeautifulSoup
 
 
-def parse_restaurant_page(restaurant_url):
-    req = Request(restaurant_url, headers={'User-Agent': 'Mozilla/5.0'})
-    restaurant_page = urlopen(req).read()
-    parsed_restaurant_html = BeautifulSoup(restaurant_page, "html.parser")
-    gmap_link = parsed_restaurant_html.find("div",
-                                            class_="box-map-links").find('a')
-    text_link = gmap_link.attrs["href"]
-    start=text_link.find('q=loc:')+6
-    coord1, coord2 = text_link[start:].split('+')
-    return coord1+','+coord2
+def write_header_gpx(target_file):
+    f = open(target_file, 'w')
+    f.write('<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n');
+    f.write('<gpx version="1.0" creator="HappyCowler" xmlns="http://www.topografix.com/GPX/1/0">\n');
+    f.close()
 
-def write_header(target_file):
+def write_footer_gpx(target_file):
+    f = open(target_file, 'a')
+    f.write('</gpx>\n')
+    f.close()
+
+
+def append_results_to_file_gpx(target_file, coordinates, names, tags, ratings,
+                               addresses, phone_numbers, opening_hours,
+                               cuisines, descriptions):
+    f = open(target_file, 'a')
+    for i in range(len(names)):
+        f.write('  <wpt lat="' + coordinates[i][0] +
+                '" lon="' + coordinates[i][1] + '">\n')
+        f.write('    <name>' + names[i] + '</name>\n')
+        f.write('        <type>Vegetarian Restaurants</type>\n')
+        f.write('        <extensions><color>#b400842b</color></extensions>\n')
+        f.write('   <desc>\n')
+        f.write('<![CDATA[')
+        f.write(addresses[i] + '\n')
+        f.write('Phone: ' + phone_numbers[i] + '\n')
+        f.write('Type: ' + tags[i] + '\n')
+        f.write('Rating: ' + ratings[i] + '\n')
+        f.write('Cuisine:  ' + cuisines[i] + '\n')
+        f.write('Business hours: ' + opening_hours[i] + '\n')
+        f.write('   <description>' + descriptions[i] + '\n')
+        f.write(']]>\n')
+        f.write('   </desc>\n')
+        f.write('  </wpt>\n')
+    f.close()
+
+
+def write_header_kml(target_file):
     f = open(target_file, 'w')
     f.write('<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n');
     f.write('<kml xmlns="http://earth.google.com/kml/2.1">\n');
     f.write('<Document>\n')
     f.close()
 
-def write_footer(target_file):
+def write_footer_kml(target_file):
     f = open(target_file, 'a')
     f.write('</Document>\n')
     f.write('</kml>\n')
     f.close()
 
-def append_results_to_file(target_file, coordinates, names, tags, ratings, addresses, phone_numbers, opening_hours, cuisines, descriptions):
+def append_results_to_file_kml(target_file, coordinates, names, tags, ratings,
+                               addresses, phone_numbers, opening_hours,
+                               cuisines, descriptions):
     f = open(target_file, 'a')
     for i in range(len(names)):
         f.write('  <Placemark>\n')
         f.write('    <name>' + names[i] + '</name>\n')
         f.write('    <Point>\n')
-        f.write('      <coordinates>' + coordinates[i] + ',0</coordinates>\n')
+        f.write('      <coordinates>' + coordinates[i][1] + "," +
+                coordinates[i][0] + ',0</coordinates>\n')
         f.write('    </Point>\n')
         f.write('   <description>\n')
         f.write('     <![CDATA[ ')
@@ -60,6 +89,51 @@ def append_results_to_file(target_file, coordinates, names, tags, ratings, addre
         f.write('   </description>\n')
         f.write('  </Placemark>\n')
     f.close()
+
+
+def write_header(target_file):
+    if target_file.endswith(".kml"):
+        write_header_kml(target_file)
+    elif target_file.endswith(".gpx"):
+        write_header_gpx(target_file)
+    else:
+        raise Exception("Unknown filetype")
+
+def write_footer(target_file):
+    if target_file.endswith(".kml"):
+        write_footer_kml(target_file)
+    elif target_file.endswith(".gpx"):
+        write_footer_gpx(target_file)
+    else:
+        raise Exception("Unknown filetype")
+
+
+def append_results_to_file(target_file, coordinates, names, tags, ratings,
+                           addresses, phone_numbers, opening_hours, cuisines,
+                           descriptions):
+    if target_file.endswith(".kml"):
+        append_results_to_file_kml(target_file, coordinates, names, tags,
+                                   ratings, addresses, phone_numbers,
+                                   opening_hours, cuisines, descriptions)
+    elif target_file.endswith(".gpx"):
+        append_results_to_file_gpx(target_file, coordinates, names, tags,
+                                   ratings, addresses, phone_numbers,
+                                   opening_hours, cuisines, descriptions)
+    else:
+        raise Exception("Unknown filetype")
+
+
+def parse_restaurant_page(restaurant_url):
+    req = Request(restaurant_url, headers={'User-Agent': 'Mozilla/5.0'})
+    restaurant_page = urlopen(req).read()
+    parsed_restaurant_html = BeautifulSoup(restaurant_page, "html.parser")
+    gmap_link = parsed_restaurant_html.find("div",
+                                            class_="box-map-links").find('a')
+    text_link = gmap_link.attrs["href"]
+    start=text_link.find('q=loc:')+6
+    lat, lon = text_link[start:].split('+')
+    return (lat, lon)
+
 
 def parse_results_page(results_url, page_no, target_file):
     req = Request(results_url + page_no, headers={'User-Agent': 'Mozilla/5.0'})
